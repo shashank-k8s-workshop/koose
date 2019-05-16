d20c57983e298f714dc3157d8b17ba858673ca46
Shashank <shashank004@gmail.com>
2019-05-16 23:26:49 +1000

    Added health-check and header propagation

---
+++ src/main/kotlin/com/koose/controllers/Constants.kt
@@ -0,0 +1,3 @@
+package com.koose.controllers
+
+const val AMAZON_TRACE_ID: String = "x-amzn-trace-id"
--- src/main/kotlin/com/koose/controllers/KooController.kt
+++ src/main/kotlin/com/koose/controllers/KooController.kt
@@ -5,7 +5,9 @@ import com.koose.services.GooService
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -24,13 +26,19 @@ class KooController {
     private val version = "0.1.0"
 
     @GetMapping("/koo")
-    fun ping(): ResponseEntity<KooResponse> {
+    fun koo(reqEntity: RequestEntity<Void>): ResponseEntity<KooResponse> {
         logger.info("koo api invoked")
-        val gooResponse = gooService.goo()
+        logger.info("traceHeader: ${reqEntity.headers[AMAZON_TRACE_ID]?.toString()}")
+        val gooResponse = gooService.goo(reqEntity.headers[AMAZON_TRACE_ID]?.toString())
         if (gooResponse == null) {
             logger.error("goo service request failed")
             return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
         }
         return ResponseEntity.ok(KooResponse(serviceName, version, listOf(gooResponse)))
+    }
+
+    @GetMapping("/health-check")
+    fun healthCheck(): ResponseEntity<String> {
+        return ResponseEntity.ok("healthy")
     }
 }
